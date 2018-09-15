c57b349dc15bcc03f9fa39e736ea4df409f88024
Gabriele Prestifilippo <gabriele.prestifilippo@gmail.com>
2018-09-15 18:22:15 +0200

    ok

--- client/src/pages/booking/index.js
+++ client/src/pages/booking/index.js
@@ -6,12 +6,14 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import { Steps, Icon } from 'antd';
+import { Steps, Icon, Collapse } from 'antd';
 import 'antd/dist/antd.css'
 import Button from '@material-ui/core/Button'
 
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
+
+const Panel = Collapse.Panel;
 
 const theme = createMuiTheme({
   palette: {
@@ -156,8 +158,42 @@ class Booking extends Component {
         return (<div><h2>Appointment has been set!</h2>
           <img src={require('../../assets/booked.png')} />
         </div>)
-
+      case 3:
+        return this.renderPrescription()
     }
+  }
+
+  renderPrescription () {
+    return (<div>
+      <h2>My Prescriptions</h2>
+    <Collapse bordered={false} defaultActiveKey={['']}>
+      <Panel header="Omeoprazol 500mg" key="1">
+        <div>
+          <h3>Dosage</h3>
+        </div>
+        2 times per day, 1 pill at breakfast, 1 pill after dinner.
+        <h3>Targets</h3>
+        burning stomach, etc
+        <h3>Side-effects</h3>
+        Nausea, vomiting, headache
+      </Panel>
+      <Panel header="NeoCitran 20mg" key="2">
+
+      </Panel>
+      <Panel header="Frutizolvin 200mg" key="3">
+
+      </Panel>
+    </Collapse>
+
+      <Button style={{margin: 30, width: '200px'}} variant="contained"
+              color="primary">
+        Pickup QR Code
+      </Button>
+      <Button style={{margin: 30, width: '200px'}} variant="contained"
+              color="primary">
+        Request home delivery
+      </Button>
+    </div>)
   }
 
   sendEmail () {
