0bd4c7f77a47addd14cee6a38d8d18b9043b011e
Gabriele Prestifilippo <gabriele.prestifilippo@gmail.com>
2018-09-15 12:03:22 +0200

    css and booking

--- client/src/pages/booking/index.js
+++ client/src/pages/booking/index.js
@@ -8,6 +8,16 @@ import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { Steps, Icon } from 'antd';
 import 'antd/dist/antd.css'
+import Button from '@material-ui/core/Button'
+
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
 
 const Step = Steps.Step;
 
@@ -53,6 +63,24 @@ class Booking extends Component {
   }
 
   handleSelect (ranges) {
+    this.setState({ranges: [ranges.selection]})
+  }
+
+  step1 () {
+    let newSteps = [{
+      description: "",
+      status: "finish",
+      title: "Availability"
+    }, {
+      description: "awaiting booking confirmation",
+      status: "process",
+      title: "Booking"
+    }, ...this.state.steps.slice(2, 4)];
+    this.setState({steps: newSteps})
+    this.setState({selectedStep: 1})
+  }
+
+  step1half () {
     let newSteps = [{
       description: "",
       status: "finish",
@@ -62,17 +90,38 @@ class Booking extends Component {
       status: "process",
       title: "Booking"
     }, ...this.state.steps.slice(2, 4)];
-
-    setTimeout(() => {
-      this.setState({steps: newSteps})
-    }, 2000)
-
-    this.setState({ranges: [ranges.selection]})
+    this.setState({steps: newSteps})
     this.setState({selectedStep: 1})
   }
 
-  iconBasic () {
-    return (<Icon type="check-circle" theme="filled" />)
+  step2 () {
+    let newSteps = [...this.state.steps.slice(0, 1), {
+      description: "",
+      status: "finish",
+      title: "Booking"
+    },
+      {
+        description: "Appointment set, waiting",
+        status: "process",
+        title: "Appointment"
+      }, ...this.state.steps.slice(3, 4)];
+    this.setState({steps: newSteps})
+    this.setState({selectedStep: 2})
+  }
+
+  step3 () {
+    let newSteps = [...this.state.steps.slice(0, 2), {
+      description: "",
+      status: "finish",
+      title: "Appointment"
+    },
+      {
+        description: "Please, get your prescription",
+        status: "process",
+        title: "Prescription"
+      }];
+    this.setState({steps: newSteps})
+    this.setState({selectedStep: 3})
   }
 
   iconCompleted () {
@@ -81,13 +130,29 @@ class Booking extends Component {
 
   renderSteps () {
     return this.state.steps.map((s, i) => {
-      return <Step title={s.title} icon={this.state.selected > i ? this.iconCompleted() : null} status={s.status}
+      return <Step key={s.title} title={s.title} icon={this.state.selected > i ? this.iconCompleted() : null}
+                   status={s.status}
                    description={s.description} />
     })
   }
 
+  renderCalendar () {
+    return this.state.selectedStep === 0 && (<div>
+      <h2>Please, select your availability</h2>
+      <DateRangePicker
+        ranges={this.state.ranges}
+        onChange={this.handleSelect}
+      />
+      {this.state.ranges[0].startDate<this.state.ranges[0].endDate && <Button style={{margin: 100, width:'40%'}} onClick={() => this.step1()} variant="contained"
+                                                                        color="primary">
+        Confirm availability
+      </Button>}
+    </div>)
+  }
+
   render () {
     return (
+      <MuiThemeProvider theme={theme}>
       <div>
 
         <div class="sideBarSteps">
@@ -97,13 +162,16 @@ class Booking extends Component {
         </div>
 
         <div class="centralBody">
-          <DateRangePicker
-            ranges={this.state.ranges}
-            onChange={this.handleSelect}
-          />
+          {this.renderCalendar()}
         </div>
 
+        <button onClick={() => this.step1()}>STEP 1</button>
+        <button onClick={() => this.step1half()}>STEP 1half</button>
+        <button onClick={() => this.step2()}>STEP 2</button>
+        <button onClick={() => this.step3()}>STEP 3</button>
+
       </div>
+      </MuiThemeProvider>
     )
   }
 }
